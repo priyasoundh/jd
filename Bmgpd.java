a9a8b58fe053c6a55a4fc47e59d315a2c237e77a
priya.s <priyasoundh@gmail.com>
2020-11-26 14:22:17 +0530

    update Bmgpd.java

--- Bmgpd.java
+++ Bmgpd.java
@@ -1,7 +1,10 @@
+import java.util.*;
 public class Bmgpd
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
 		System.out.println("working properly");
 	}
 }
